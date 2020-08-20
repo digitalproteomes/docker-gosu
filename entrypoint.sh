1b410ef052dab0764187feec06826c23fc82998f
Patrick Pedrioli <pedrioli@gmail.com>
2020-08-20 08:44:33 +0000

    Linted

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@
 USER_ID=${LOCAL_USER_ID:-1000}
 GROUP_ID=${LOCAL_GROUP_ID:-1000}
 
-usermod -u $USER_ID user
-groupmod -g $GROUP_ID userg
+usermod -u "$USER_ID" user
+groupmod -g "$GROUP_ID" userg
 usermod -a -G sudo user
 
 ## Execute CMD passed by the user when statrting the image
